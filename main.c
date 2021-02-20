1ae271a6325f2632c9293bb11842eeea49d9ea7d
sven-eliasen <jo.pettersen@pm.me>
2021-02-20 04:20:33 -0800

    main window gui success

--- main.c
+++ main.c
@@ -59,8 +59,8 @@ int main(int argc, char **argv) {
 
     GtkBuilder *builder = gtk_builder_new ();
 
-    gtk_builder_add_from_file (builder, "../glade/window.glade", NULL);
-    GtkWidget *win = (GtkWidget *) gtk_builder_get_object (builder,"window1");
+    gtk_builder_add_from_file (builder, "../glade/main.glade", NULL);
+    GtkWidget *win = (GtkWidget *) gtk_builder_get_object (builder,"main_window");
     gtk_builder_connect_signals(builder, NULL);
 
     gtk_widget_show_all (win);
